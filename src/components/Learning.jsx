30749c1b1868989f70894105d6c055899d1f9593
Bertoldo Klinger <bertoldokbrj@gmail.com>
2023-08-07 19:57:22 -0300

    feat: add Learning component responsiveness

--- src/components/Learning.jsx
+++ src/components/Learning.jsx
@@ -2,8 +2,9 @@
 export function Learning() {
 
   return (
-    <div className="bg-learning-mobile md:bg-learning-desktop bg-cover bg-center h-screen w-full">
-      <button className="
+    <div className="flex  justify-center bg-learning-mobile md:bg-learning-desktop bg-cover bg-center h-screen w-full">
+      <div className=" w-[22rem] max-w-[40rem] notebook:w-[26rem]">
+        <button className="
               font-poppins
               font-bold
             bg-button-bg
@@ -11,30 +12,64 @@ export function Learning() {
             text-white
               text-center
               text-[1.6rem]
-              px-8
-              py-1
+              px-10
+              py-2
               rounded-xl
-              ml-5
-              mt-[45.8rem]
+              whitespace-nowrap
+              
+              mt-[49.5rem]
 
               
               notebook:mt-[34.5rem]
-              notebook:ml-[28.5rem]
+              
               notebook:whitespace-nowrap
               notebook:text-3xl
               notebook:py-2
 
-              fullhd:mt-[52rem]
-              fullhd:ml-[46rem]
+              fullhd:mt-[51.5rem]
+              
               fullhd:whitespace-nowrap
               fullhd:text-4xl
               fullhd:p-3
               fullhd:px-5
       ">
-        QUERO ME INSCREVER
-      </button>
+          QUERO ME INSCREVER
+        </button>
+      </div>
+      
     </div>
   );
 }
-// iphonexr:mt-[51.25rem]
-              // iphonexr:ml-10
+
+
+
+
+
+      //         <button className="
+      //         font-poppins
+      //         font-bold
+      //       bg-button-bg
+      //         shadow-2xl
+      //       text-white
+      //         text-center
+      //         text-[1.6rem]
+      //         px-8
+      //         py-1
+      //         rounded-xl
+      //         ml-5
+      //         mt-[45.8rem]
+
+              
+      //         notebook:mt-[34.5rem]
+      //         notebook:ml-[28.5rem]
+      //         notebook:whitespace-nowrap
+      //         notebook:text-3xl
+      //         notebook:py-2
+
+      //         fullhd:mt-[52rem]
+      //         fullhd:ml-[46rem]
+      //         fullhd:whitespace-nowrap
+      //         fullhd:text-4xl
+      //         fullhd:p-3
+      //         fullhd:px-5
+      // ">
